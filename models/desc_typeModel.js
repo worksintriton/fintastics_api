c618c8b70225a775f87862685618e664f7aacf23
srini amico <srinivasan.n@amicosolutions.com>
2022-02-15 18:00:03 +0530

    icon field added

--- models/desc_typeModel.js
+++ models/desc_typeModel.js
@@ -6,6 +6,7 @@ var desc_typeSchema = new mongoose.Schema({
   desc_type :  String,
   date_and_time : String,
   delete_status : Boolean,
+  icon: String
 });
 desc_typeSchema.plugin(timestamps);
 mongoose.model('desc_type', desc_typeSchema);
